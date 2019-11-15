7ca0024987c651965e3a81a447dcdfcc0562c8ab
syedsaadahmed <syedsaadahmed2094@yahoo.com>
2019-11-15 01:58:05 +0100

    update in script

--- httpclient.py
+++ httpclient.py
@@ -1,62 +1,18 @@
-
-# import socket
-# s=socket.socket()
-# host=socket.gethostbyname('www.google.com')
-# port=80
-# s.connect((host,port))
-# s.sendall(b"GET /\r\n")
-# # output = "GET /\r\n"
-# # s.sendall(output.encode('utf-8'))
-# val = s.recv(100000000)
-# # Split off the HTTP headers
-# #val = val.split('\r\n\r\n',1)[1]
-# # #print(val)
-# while (len(val) > 0):
-#     #print(val)
-#     val = s.recv(10000)
-# print(type(val.decode("utf-8")))
-# val = val.decode("utf-8")
-# headers = val.split('\r\n\r\n', 1)[0]
-# body = val.split('\r\n\r\n', 1)[-1]
-# print(body)
-
-
-
-# import socket
-# s=socket.socket()
-# host=socket.gethostbyname('west.uni-koblenz.de/')
-# port=80
-# s.connect((host,port))
-# #s.sendall(b"GET /\r\n")
-# s.sendall(b'GET /studying/ws1819/webscience HTTP/1.1\r\n\r\n')
-# #val = s.recv(100000000)
-# def recvall(sock):
-#     data = b''
-#     bufsize = 409600
-#     while True:
-#         packet = s.recv(bufsize)
-#         data += packet
-#         if len(packet) < bufsize:
-#             break
-#     return data
-
-# data = recvall(s)
-# print(type(data.decode("utf-8")))
-# data = data.decode("utf-8")
-# headers = data.split('\r\n\r\n', 1)[0]
-# body = data.split('\r\n\r\n', 1)[-1]
-# print(headers)
-
-
-
-
 import socket
+import sys
 
-server_address = ('west.uni-koblenz.de', 80)
-message  = b'GET /studying/ws1819/webscience HTTP/1.1\r\n'
-message += b'Host: west.uni-koblenz.de:80\r\n'
-message += b'Connection: close\r\n'
-message += b'\r\n'
+print('---------------------------')
+url = sys.argv[1]
+host = url.split("//")[-1].split("/")[0]
+a = url.split("//")[1].split("/")[1:]
+seperator = '/'
+query_param = seperator.join(a)
+
+server_address = (host, 80)
+message  = ('GET /' + query_param + ' HTTP/1.1\r\n').encode()
+message += ('Host: ' + host + ':80\r\n').encode()
+message += ('Connection: close\r\n').encode()
+message += ('\r\n').encode()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(server_address)
